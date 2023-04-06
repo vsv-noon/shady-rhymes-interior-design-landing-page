4bcbebb5de161f5bffe616a62451a8c5ea07e560
ViacheslavAnisimov <forvsn@gmail.com>
2023-04-06 22:43:24 +0700

    added slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {    
+    // effect: "fade",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+  });
